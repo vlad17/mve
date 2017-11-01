6eddcdfddbc94a6270a01fc3fb7f44c1395c6882
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2017-11-01 09:38:09 -0700

    pylint cheethah_env.py

--- mpc_bootstrap/cheetah_env.py
+++ mpc_bootstrap/cheetah_env.py
@@ -1,8 +1,16 @@
-import numpy as np
+"""White-box HalfCheetah environment which reveals relevant physical info"""
+
 from gym import utils
 from gym.envs.mujoco import mujoco_env
+import numpy as np
 
+from utils import inherit_doc
+
+
+@inherit_doc
 class HalfCheetahEnvNew(mujoco_env.MujocoEnv, utils.EzPickle):
+    """Provided code from HW4 for creating a white-box HalfCheetah env"""
+
     def __init__(self):
         mujoco_env.MujocoEnv.__init__(self, 'half_cheetah.xml', 1)
         utils.EzPickle.__init__(self)
@@ -13,21 +21,24 @@ class HalfCheetahEnvNew(mujoco_env.MujocoEnv, utils.EzPickle):
         xposafter = self.model.data.qpos[0, 0]
         ob = self._get_obs()
         reward_ctrl = - 0.1 * np.square(action).sum()
-        reward_run = (xposafter - xposbefore)/self.dt
+        reward_run = (xposafter - xposbefore) / self.dt
         reward = reward_ctrl + reward_run
         done = False
-        return ob, reward, done, dict(reward_run=reward_run, reward_ctrl=reward_ctrl)
+        info = dict(reward_run=reward_run, reward_ctrl=reward_ctrl)
+        return ob, reward, done, info
 
     def _get_obs(self):
         return np.concatenate([
             self.model.data.qpos.flat[1:],
             self.model.data.qvel.flat,
             self.get_body_com("torso").flat,
+            # TODO: find out why the below was filtered by HW4
             # self.get_body_comvel("torso").flat,
         ])
 
     def reset_model(self):
-        qpos = self.init_qpos + self.np_random.uniform(low=-.1, high=.1, size=self.model.nq)
+        qpos = self.init_qpos + \
+            self.np_random.uniform(low=-.1, high=.1, size=self.model.nq)
         qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
         self.set_state(qpos, qvel)
         return self._get_obs()
