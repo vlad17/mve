7c75d5c6258984c845d6f6d4a7d402f33d64e997
Vladimir Feinberg <vlad17@users.noreply.github.com>
2017-11-10 13:31:57 -0800

    [improvement] Revamp benchscripts (#78)
    * update scripts to new logging metrics; rm hard2
    
    * fix easy-cost.sh bug: missing --outfile
    
    * hard cost add zero agent plotting

--- scripts/easy-cost.sh
+++ scripts/easy-cost.sh
@@ -1,43 +1,49 @@
 #! /usr/bin/env bash
 # Run the basic configurations for easy half-cheetah
 # Should be run from the repo root
-# Saves runs in ./data and the following images in ./report:
 #
 # ./scripts/easy-cost
 
-python mpc_bootstrap/main.py mpc --onpol_iters 15 --exp_name mpc-easy  --seed 1 5 10 --verbose --env_name hc-easy
-python mpc_bootstrap/main.py delta_bootstrap --onpol_iters 15 --exp_name deterministic-easy --seed 1 5 10 --verbose --con_depth 5 --con_width 32 --con_epochs 100 --con_learning_rate 1e-3 --con_batch_size 512 --explore_std 0 --env_name hc-easy
-python mpc_bootstrap/main.py gaussian_bootstrap --onpol_iters 15 --exp_name stochastic-easy --seed 1 5 10 --verbose --con_depth 5 --con_width 32 --con_epochs 100 --con_learning_rate 1e-3 --con_batch_size 512 --env_name hc-easy
-python mpc_bootstrap/main.py gaussian_bootstrap --onpol_iters 15 --exp_name pure-easy --seed 1 5 10 --verbose --con_depth 5 --con_width 32 --con_epochs 100 --con_learning_rate 1e-3 --con_batch_size 512 --no_extra_explore --env_name hc-easy
+python mpc_bootstrap/main.py mpc --onpol_iters 15 --exp_name mpc --seed 1 --verbose --env_name hc-easy
+python mpc_bootstrap/main.py zero_bootstrap --onpol_iters 15 --exp_name zero --seed 1 --verbose --env_name hc-easy
+python mpc_bootstrap/main.py delta_bootstrap --onpol_iters 15 --exp_name delta --seed 1 --verbose --env_name hc-easy
+python mpc_bootstrap/main.py gaussian_bootstrap --onpol_iters 15 --exp_name gaussian --seed 1 --verbose --env_name hc-easy
+python mpc_bootstrap/main.py gaussian_bootstrap --onpol_iters 15 --exp_name no-explore-gaussian --seed 1 --verbose --no_extra_explore --env_name hc-easy
 
 python mpc_bootstrap/plot.py \
-    "data/mpc-easy_hc-easy:AverageReturn:MPC" \
-    'data/deterministic-easy_hc-easy:AverageReturn:$\delta$-BMPC' \
-    "data/stochastic-easy_hc-easy:AverageReturn:Gaussian BMPC" \
-    "data/pure-easy_hc-easy:AverageReturn:No-explore BMPC" \
+    "data/mpc_hc-easy:AverageReturn:MPC" \
+    "data/zero_hc-easy:AverageReturn:Zero BMPC" \
+    'data/delta_hc-easy:AverageReturn:$\delta$-BMPC' \
+    "data/gaussian_hc-easy:AverageReturn:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-easy:AverageReturn:No-explore Gaussian BMPC" \
     --outfile report/easy-AverageReturn.pdf --yaxis "average rollout return"
 python mpc_bootstrap/plot.py \
-    "data/mpc-easy_hc-easy:AverageReturn:MPC" \
-    'data/deterministic-easy_hc-easy:LearnerAverageReturn:$\delta$-BMPC' \
-    "data/stochastic-easy_hc-easy:LearnerAverageReturn:Gaussian BMPC" \
-    "data/pure-easy_hc-easy:LearnerAverageReturn:No-explore BMPC" \
+    "data/mpc_hc-easy:AverageReturn:MPC" \
+    "data/zero_hc-easy:LearnerAverageReturn:Zero BMPC" \
+    'data/delta_hc-easy:LearnerAverageReturn:$\delta$-BMPC' \
+    "data/gaussian_hc-easy:LearnerAverageReturn:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-easy:LearnerAverageReturn:No-explore Gaussian BMPC" \
     --outfile report/easy-LearnerAverageReturn.pdf --yaxis "average rollout return"
 python mpc_bootstrap/plot.py \
-    "data/mpc-easy_hc-easy:DynamicsMSE:MPC" \
-    'data/deterministic-easy_hc-easy:DynamicsMSE:$\delta$-BMPC' \
-    "data/stochastic-easy_hc-easy:DynamicsMSE:Gaussian BMPC" \
-    "data/pure-easy_hc-easy:DynamicsMSE:No-explore BMPC" \
+    "data/mpc_hc-easy:DynamicsMSE:MPC" \
+    "data/zero_hc-easy:DynamicsMSE:Zero BMPC" \
+    'data/delta_hc-easy:DynamicsMSE:$\delta$-BMPC' \
+    "data/gaussian_hc-easy:DynamicsMSE:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-easy:DynamicsMSE:No-explore Gaussian BMPC" \
     --outfile report/easy-DynamicsMSE.pdf --yaxis "dynamics MSE" \
     --drop_iterations 5
 python mpc_bootstrap/plot.py \
-    "data/mpc-easy_hc-easy:RewardMSE:MPC" \
-    "data/stochastic-easy_hc-easy:RewardMSE:Gaussian BMPC" \
-    "data/pure-easy_hc-easy:RewardMSE:No-explore BMPC" \
-    --outfile report/easy-RewardMSE.pdf --yaxis "reward MSE"
-#     'data/deterministic-easy_hc-easy:RewardMSE:$\delta$-BMPC' \
+    "data/mpc_hc-easy:StandardizedRewardMSE:MPC" \
+    "data/zero-easy:StandardizedRewardMSE:Gaussian Zero" \
+    'data/delta_hc-easy:StandardizedRewardMSE:$\delta$-BMPC' \
+    "data/gaussian-easy:StandardizedRewardMSE:Gaussian BMPC" \
+    "no-explore-gaussian_hc-easy:StandardizedRewardMSE:No-explore Gaussian BMPC" \
+    --outfile report/easy-StandardizedRewardMSE.pdf --yaxis "reward MSE"
 python mpc_bootstrap/plot.py \
-    "data/mpc-easy_hc-easy:AverageRewardBias:MPC" \
-    "data/stochastic-easy_hc-easy:AverageRewardBias:Gaussian BMPC" \
-    "data/pure-easy_hc-easy:AverageRewardBias:No-explore BMPC" \
-    --outfile report/easy-AverageRewardBias.pdf --yaxis "reward MSE"
-#    'data/deterministic-easy_hc-easy:AverageRewardBias:$\delta$-BMPC' \
+    "data/mpc_hc-easy:StandardizedRewardBias:MPC" \
+    "data/zero_hc-easy:StandardizedRewardBias:Zero BMPC" \
+    'data/delta_hc-easy:StandardizedRewardBias:$\delta$-BMPC' \
+    "data/gaussian_hc-easy:StandardizedRewardBias:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-easy:StandardizedRewardBias:No-explore Gaussian BMPC" \
+    --outfile report/easy-StandardizedRewardBias.pdf --yaxis "reward MSE"
+
--- scripts/hard-cost.sh
+++ scripts/hard-cost.sh
@@ -1,45 +1,50 @@
 #! /usr/bin/env bash
 # Run the basic configurations for hard half-cheetah
 # Should be run from the repo root
-# Saves runs in ./data and the following images in ./report:
 #
 # ./scripts/hard-cost.sh
 
-python mpc_bootstrap/main.py mpc --onpol_iters 20 --exp_name mpc-hard  --seed 1 5 10 --verbose --env_name hc-hard
-python mpc_bootstrap/main.py delta_bootstrap --onpol_iters 20 --exp_name deterministic-hard --seed 1 5 10 --verbose --con_depth 5 --con_width 32 --con_epochs 100 --con_learning_rate 1e-3 --con_batch_size 512 --explore_std 0 --env_name hc-hard
-python mpc_bootstrap/main.py gaussian_bootstrap --onpol_iters 20 --exp_name stochastic-hard --seed 1 5 10 --verbose --con_depth 5 --con_width 32 --con_epochs 100 --con_learning_rate 1e-3 --con_batch_size 512 --env_name hc-hard
-python mpc_bootstrap/main.py gaussian_bootstrap --onpol_iters 20 --exp_name pure-hard --seed 1 5 10 --verbose --con_depth 5 --con_width 32 --con_epochs 100 --con_learning_rate 1e-3 --con_batch_size 512 --no_extra_explore --env_name hc-hard
+python mpc_bootstrap/main.py mpc --onpol_iters 15 --exp_name mpc  --seed 1 --verbose --env_name hc-hard
+python mpc_bootstrap/main.py zero_bootstrap --onpol_iters 15 --exp_name zero --seed 1 --verbose --env_name hc-hard
+python mpc_bootstrap/main.py delta_bootstrap --onpol_iters 15 --exp_name delta --seed 1 --verbose --explore_std 0 --env_name hc-hard
+python mpc_bootstrap/main.py gaussian_bootstrap --onpol_iters 15 --exp_name gaussian --seed 1 --verbose --env_name hc-hard
+python mpc_bootstrap/main.py gaussian_bootstrap --onpol_iters 15 --exp_name no-explore-gaussian --seed 1 --verbose --no_extra_explore --env_name hc-hard
 
 python mpc_bootstrap/plot.py \
-    "data/mpc-hard_hc-hard:AverageReturn:MPC" \
-    'data/deterministic-hard_hc-hard:AverageReturn:$\delta$-BMPC' \
-    "data/stochastic-hard_hc-hard:AverageReturn:Gaussian BMPC" \
-    "data/pure-hard_hc-hard:AverageReturn:No-explore BMPC" \
+    "data/mpc_hc-hard:AverageReturn:MPC" \
+    "data/zero_hc-hard:AverageReturn:Zero BMPC" \
+    'data/delta_hc-hard:AverageReturn:$\delta$-BMPC' \
+    "data/gaussian_hc-hard:AverageReturn:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-hard:AverageReturn:No-explore BMPC" \
     --outfile report/hard-AverageReturn.pdf --yaxis "average rollout return"
 python mpc_bootstrap/plot.py \
-    "data/mpc-hard_hc-hard:AverageReturn:MPC" \
-    'data/deterministic-hard_hc-hard:LearnerAverageReturn:$\delta$-BMPC' \
-    "data/stochastic-hard_hc-hard:LearnerAverageReturn:Gaussian BMPC" \
-    "data/pure-hard_hc-hard:LearnerAverageReturn:No-explore BMPC" \
+    "data/mpc_hc-hard:AverageReturn:MPC" \
+    "data/zero_hc-hard:LearnerAverageReturn:Zero BMPC" \
+    'data/delta_hc-hard:LearnerAverageReturn:$\delta$-BMPC' \
+    "data/gaussian_hc-hard:LearnerAverageReturn:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-hard:LearnerAverageReturn:No-explore BMPC" \
     --outfile report/hard-LearnerAverageReturn.pdf --yaxis "average rollout return"
 python mpc_bootstrap/plot.py \
-    "data/mpc-hard_hc-hard:DynamicsMSE:MPC" \
-    'data/deterministic-hard_hc-hard:DynamicsMSE:$\delta$-BMPC' \
-    "data/stochastic-hard_hc-hard:DynamicsMSE:Gaussian BMPC" \
-    "data/pure-hard_hc-hard:DynamicsMSE:No-explore BMPC" \
+    "data/mpc_hc-hard:DynamicsMSE:MPC" \
+    "data/zero_hc-hard:DynamicsMSE:Zero BMPC" \
+    'data/delta_hc-hard:DynamicsMSE:$\delta$-BMPC' \
+    "data/gaussian_hc-hard:DynamicsMSE:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-hard:DynamicsMSE:No-explore BMPC" \
     --outfile report/hard-DynamicsMSE.pdf --yaxis "dynamics MSE" \
     --drop_iterations 5
 python mpc_bootstrap/plot.py \
-    "data/mpc-hard_hc-hard:RewardMSE:MPC" \
-    'data/deterministic-hard_hc-hard:RewardMSE:$\delta$-BMPC' \
-    "data/stochastic-hard_hc-hard:RewardMSE:Gaussian BMPC" \
-    "data/pure-hard_hc-hard:RewardMSE:No-explore BMPC" \
-    --outfile report/hard-RewardMSE.pdf --yaxis "reward MSE" \
+    "data/mpc_hc-hard:StandardizedRewardMSE:MPC" \
+    "data/zero_hc-hard:StandardizedRewardMSE:Zero BMPC" \
+    'data/delta_hc-hard:StandardizedRewardMSE:$\delta$-BMPC' \
+    "data/gaussian_hc-hard:StandardizedRewardMSE:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-hard:StandardizedRewardMSE:No-explore BMPC" \
+    --outfile report/hard-StandardizedRewardMSE.pdf --yaxis "reward MSE" \
     --drop_iterations 5
 python mpc_bootstrap/plot.py \
-    "data/mpc-hard_hc-hard:AverageRewardBias:MPC" \
-    'data/deterministic-hard_hc-hard:AverageRewardBias:$\delta$-BMPC' \
-    "data/stochastic-hard_hc-hard:AverageRewardBias:Gaussian BMPC" \
-    "data/pure-hard_hc-hard:AverageRewardBias:No-explore BMPC" \
-    --outfile report/hard-AverageRewardBias.pdf --yaxis "reward MSE" \
+    "data/mpc_hc-hard:StandardizedRewardBias:MPC" \
+    "data/zero_hc-hard:StandardizedRewardBias:Zero BMPC" \
+    'data/delta_hc-hard:StandardizedRewardBias:$\delta$-BMPC' \
+    "data/gaussian_hc-hard:StandardizedRewardBias:Gaussian BMPC" \
+    "data/no-explore-gaussian_hc-hard:StandardizedRewardBias:No-explore BMPC" \
+    --outfile report/hard-StandardizedRewardBias.pdf --yaxis "reward bias" \
     --drop_iterations 5
