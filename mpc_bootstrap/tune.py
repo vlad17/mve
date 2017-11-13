3b796da33d67ec0c7e0e58debb555c80b8d68c26
SamvitJ <samvit.jain@gmail.com>
2017-11-12 22:44:52 -0800

    Address PR comments.

--- mpc_bootstrap/tune.py
+++ mpc_bootstrap/tune.py
@@ -1,4 +1,4 @@
-"""Generate MPC rollouts."""
+"""Search for best hyperparameters."""
 
 import os
 import json
@@ -7,15 +7,17 @@ import copy
 # import mujoco for weird dlopen reasons
 import mujoco_py # pylint: disable=unused-import
 import tensorflow as tf
+import ray
 
 from dynamics_flags import DynamicsFlags
 from mpc_flags import MpcFlags
 from experiment_flags import ExperimentFlags
-from flags import (convert_flags_to_json, parse_args)
+from warmup import WarmupFlags
+from flags import convert_flags_to_json as flags_to_json
+from flags import parse_args
 from utils import (make_data_directory, seed_everything)
 import log
 import logz
-import ray
 from main_mpc import _train as train_mpc
 
 def _main(args):
@@ -26,32 +28,30 @@ def _main(args):
     datadir = "{}_{}".format(exp_name, env_name)
     logdir = make_data_directory(datadir)
 
-    ray.init() # TODO: supply num_gpus as cmd line arg
+    ray.init() # TODO: supply redis ip as cmd line arg
+
+    @ray.remote
+    def _train_mpc(_logdir, seed, i):
+        # Save params to disk.
+        logdir_seed = os.path.join(_logdir, str(seed))
+        logdir_seed_proc = os.path.join(logdir_seed, str(i))
+        logz.configure_output_dir(logdir_seed_proc)
+        with open(os.path.join(logdir_seed, 'params.json'), 'w') as f:
+            json.dump(flags_to_json(args), f, sort_keys=True, indent=4)
+
+        args_inst = copy.deepcopy(args)
+        args_inst.mpc.onpol_paths = 5 + (5 * i)
+        log.debug('testing: --onpol_paths', args_inst.mpc.onpol_paths)
+        train_mpc(args_inst)
 
     for seed in args.experiment.seed:
-        # Save params to disk.
-        logdir_seed = os.path.join(logdir, str(seed))
-        logz.configure_output_dir(logdir_seed)
-        with open(os.path.join(logdir_seed, 'params.json'), 'w') as f:
-            json.dump(convert_flags_to_json(args), f, sort_keys=True, indent=4)
-
-        @ray.remote
-        def _train_mpc_inst(logdir_root, i):
-            logdir_seed_proc = os.path.join(logdir_root, str(i))
-            logz.configure_output_dir(logdir_seed_proc)
-
-            args_inst = copy.deepcopy(args)
-            args_inst.mpc.onpol_paths = 5 + (5 * i)
-            print('testing: --onpol_paths', args_inst.mpc.onpol_paths)
-            train_mpc(args_inst)
-
         # Run experiment.
         g = tf.Graph()
         with g.as_default():
             seed_everything(seed)
-            ray.get([_train_mpc_inst.remote(logdir_seed, i) for i in range(3)])
+            ray.get([_train_mpc.remote(logdir, seed, i) for i in range(3)])
 
 
 if __name__ == "__main__":
-    _args = parse_args([ExperimentFlags, MpcFlags, DynamicsFlags])
+    _args = parse_args([ExperimentFlags, MpcFlags, DynamicsFlags, WarmupFlags])
     _main(_args)
--- scripts/easy-cost.sh
+++ scripts/easy-cost.sh
@@ -46,5 +46,3 @@ python mpc_bootstrap/plot.py \
     "data/gaussian_hc-easy:StandardizedRewardBias:Gaussian BMPC" \
     "data/no-explore-gaussian_hc-easy:StandardizedRewardBias:No-explore Gaussian BMPC" \
     --outfile report/easy-StandardizedRewardBias.pdf --yaxis "reward MSE"
-
-python mpc_bootstrap/tune.py  --onpol_iters 15 --exp_name mpc --seed 1 --verbose --env_name hc-easy
