0442c8269552aef8b44b205d971e314ad112210d
Michael Whittaker <mwhittaker@users.noreply.github.com>
2017-11-22 12:07:57 -0800

    Fixed buggy logging. Fixed #62. (#148)
    Before, the path name and line number in a log entry were always the
    same:
    
        [2017-11-06 21:56:57,247 /data/vladf/mpc-bootstrap/mpc_bootstrap/log.py:44]
    
    Now, we log the path name and line number of the function which calls
    log.debug. For example:
    
        [2017-11-21 17:48:47,699 mpc_bootstrap/main_random_policy.py:61]

--- mpc_bootstrap/log.py
+++ mpc_bootstrap/log.py
@@ -23,22 +23,57 @@ And then from any other file, run something this:
     debug("Iteration {} of {}", 1, num_iters)
 """
 
+import inspect
 import logging
 
-logger = logging.getLogger("mpc_bootstrap")
+
+class _StackCrawlingFormatter(logging.Formatter):
+    """
+    If we configure a python logger with the format string
+    "%(pathname):%(lineno): %(message)", messages logged via `log.debug` will
+    be prefixed with the path name and line number of the code that called
+    `log.debug`. Unfortunately, when a `log.debug` call is wrapped in a helper
+    function (e.g. debug below), the path name and line number is always that
+    of the helper function, not the function which called the helper function.
+
+    A _StackCrawlingFormatter is a hack to log a different pathname and line
+    number. Simply set the `pathname` and `lineno` attributes of the formatter
+    before you call `log.debug`. See `debug` below for an example.
+    """
+    def __init__(self, format_str):
+        super().__init__(format_str)
+        self.pathname = None
+        self.lineno = None
+
+    def format(self, record):
+        s = super().format(record)
+        if self.pathname is not None:
+            s = s.replace('{pathname}', self.pathname)
+        if self.lineno is not None:
+            s = s.replace('{lineno}', str(self.lineno))
+        return s
+
+
+_LOGGER = logging.getLogger("mpc_bootstrap")
+_FORMAT_STRING = "[%(asctime)-15s {pathname}:{lineno}] %(message)s"
+_FORMATTER = _StackCrawlingFormatter(_FORMAT_STRING)
 
 
 def init(verbose):
     """Initialize the logger."""
-    format_ = "[%(asctime)-15s %(pathname)s:%(lineno)-3s] %(message)s"
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(format_))
-    logger.propagate = False
-    logger.addHandler(handler)
+    handler.setFormatter(_FORMATTER)
+    _LOGGER.propagate = False
+    _LOGGER.addHandler(handler)
     if verbose:
-        logger.setLevel(logging.DEBUG)
+        _LOGGER.setLevel(logging.DEBUG)
 
 
 def debug(s, *args):
     """debug(s, x1, ..., xn) logs s.format(x1, ..., xn)."""
-    logger.debug(s.format(*args))
+    # Get the path name and line number of the function which called us.
+    previous_frame = inspect.currentframe().f_back
+    pathname, lineno, _, _, _ = inspect.getframeinfo(previous_frame)
+    _FORMATTER.pathname = pathname
+    _FORMATTER.lineno = lineno
+    _LOGGER.debug(s.format(*args))
