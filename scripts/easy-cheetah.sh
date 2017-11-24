991a708d9cbeef518e1c3b5da848221e1ae4fd16
Vladimir Feinberg <vlad17@users.noreply.github.com>
2017-11-24 12:33:33 -0800

    [cleanup] cleaned up easy-cheetah plots (#155)
    * updated easy-cheetah script file for better plotting
    
    * renamed BMPC to RS MPC, made everything 4 seeds
    
    * added easy-cheetah run results

--- scripts/easy-cheetah.sh
+++ scripts/easy-cheetah.sh
@@ -5,46 +5,50 @@
 # ./scripts/easy-cheetah.sh
 #
 # This generates the following images in the report/ folder:
-#   hard-cheetah-return.pdf - returns for 1-3
-#   hard-cheetah-learner-return.pdf - for 2-4
-#   hard-cheetah-reward-bias.pdf - for 1-3
-#   hard-cheetah-reward-mse.pdf - for 1-3
-#   hard-cheetah-dynamics-mse.pdf - for 1-3
+#   easy-cheetah-return.pdf - returns for 1-3
+#   easy-cheetah-learner-return.pdf - for 2-4
+#   easy-cheetah-reward-bias.pdf - for 1-3
+#   easy-cheetah-reward-mse.pdf - for 1-3
+#   easy-cheetah-dynamics-mse.pdf - for 1-3
 #
 # (1) Uniform-sampling MPC (2) delta-BMPC (3) 0-BMPC (4) random
 
 
-python mpc_bootstrap/main_bootstrapped_mpc.py delta --verbose --exp_name delta --env_name hc-easy --onpol_iters 20 --seed 1 5 10
-python mpc_bootstrap/main_bootstrapped_mpc.py zero --verbose --exp_name zero --env_name hc-easy --onpol_iters 20 --seed 1 5 10
-python mpc_bootstrap/main_mpc.py --verbose --exp_name mpc --env_name hc-easy --onpol_iters 20 --seed 1 5 10
-python mpc_bootstrap/main_random_policy.py --verbose --exp_name random --env_name hc-easy --num_paths 20 --seed 1 5 10
+python mpc_bootstrap/main_bootstrapped_mpc.py delta --verbose --exp_name delta --env_name hc-easy --onpol_iters 20 --seed 1 5 10 15
+python mpc_bootstrap/main_bootstrapped_mpc.py zero --verbose --exp_name zero --env_name hc-easy --onpol_iters 20 --seed 1 5 10 15
+python mpc_bootstrap/main_mpc.py --verbose --exp_name mpc --env_name hc-easy --onpol_iters 20 --seed 1 5 10 15
+python mpc_bootstrap/main_random_policy.py --verbose --exp_name random --env_name hc-easy --num_paths 20 --seed 1 5 10 15
 
 python mpc_bootstrap/plot.py \
-       'data/delta_hc-easy:avg-return:$\delta$-BMPC' \
-       'data/mpc_hc-easy:avg-return:Uniform MPC' \
-       'data/zero_hc-easy:avg-return:$0$-BMPC (learner)' \
+       'data/delta_hc-easy:avg-return:$\delta$ RS MPC' \
+       'data/mpc_hc-easy:avg-return:Uniform RS MPC' \
+       'data/zero_hc-easy:avg-return:$0$ RS MPC' \
        --yaxis "10-episode avg return" --outfile report/easy-cheetah-return.pdf
 
 python mpc_bootstrap/plot.py \
-       'data/delta_hc-easy:avg-learner-return:$\delta$-BMPC' \
-       'data/zero_hc-easy:avg-learner-return:$0$-BMPC (learner)' \
+       'data/delta_hc-easy:avg-learner-return:$\delta$ RS MPC' \
+       'data/zero_hc-easy:avg-learner-return:$0$ RS MPC' \
        --hlines 'data/random_hc-easy:avg-return:random' \
        --yaxis "10-episode avg return" --outfile report/easy-cheetah-learner-return.pdf
 
+
 python mpc_bootstrap/plot.py \
-       'data/delta_hc-easy:standardized-reward-bias:$\delta$-BMPC' \
-       'data/mpc_hc-easy:standardized-reward-bias:Uniform MPC' \
-       'data/zero_hc-easy:standardized-reward-bias:$0$-BMPC (learner)' \
+       'data/delta_hc-easy:standardized-reward-bias:$\delta$ RS MPC' \
+       'data/mpc_hc-easy:standardized-reward-bias:Uniform RS MPC' \
+       'data/zero_hc-easy:standardized-reward-bias:$0$ RS MPC' \
+       --drop_iterations 7 \
        --yaxis "10-episode standardized reward bias" --outfile report/easy-cheetah-reward-bias.pdf
 
 python mpc_bootstrap/plot.py \
-       'data/delta_hc-easy:standardized-reward-mse:$\delta$-BMPC' \
-       'data/mpc_hc-easy:standardized-reward-mse:Uniform MPC' \
-       'data/zero_hc-easy:standardized-reward-mse:$0$-BMPC (learner)' \
+       'data/delta_hc-easy:standardized-reward-mse:$\delta$ RS MPC' \
+       'data/mpc_hc-easy:standardized-reward-mse:Uniform RS MPC' \
+       'data/zero_hc-easy:standardized-reward-mse:$0$ RS MPC' \
+       --drop_iterations 7 \
        --yaxis "10-episode standardized reward MSE" --outfile report/easy-cheetah-reward-mse.pdf
 
 python mpc_bootstrap/plot.py \
-       'data/delta_hc-easy:dynamics-mse:$\delta$-BMPC' \
-       'data/mpc_hc-easy:dynamics-mse:Uniform MPC' \
-       'data/zero_hc-easy:dynamics-mse:$0$-BMPC (learner)' \
+       'data/delta_hc-easy:dynamics-mse:$\delta$ RS MPC' \
+       'data/mpc_hc-easy:dynamics-mse:Uniform RS MPC' \
+       'data/zero_hc-easy:dynamics-mse:$0$ RS MPC' \
+       --drop_iterations 7 \
        --yaxis "10-episode dynamics MSE" --outfile report/easy-cheetah-dynamics-mse.pdf
