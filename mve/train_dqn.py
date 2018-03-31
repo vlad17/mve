37935ff4e7047750f76a38bfa9c25e559c725e6f
Brijen Thananjeyan <brijenthananjeyan@gmail.com>
2018-03-31 03:06:59 -0700

    missing import, cleanup

--- mve/train_dqn.py
+++ mve/train_dqn.py
@@ -1,15 +1,6 @@
 import tensorflow.contrib.layers as layers
-import deepq
+import deepq, sys
 from deepq.simple import run_experiment
 
-
-def model(inpt, num_actions, scope, reuse=False):
-    """This model takes as input an observation and returns values of all actions."""
-    with tf.variable_scope(scope, reuse=reuse):
-        out = inpt
-        out = layers.fully_connected(out, num_outputs=64, activation_fn=tf.nn.tanh)
-        out = layers.fully_connected(out, num_outputs=num_actions, activation_fn=None)
-        return out
-
 if __name__ == '__main__':
-    run_experiment(model, horizon=int(sys.argv[1]))
+    run_experiment(model=deepq.models.mlp([64]), horizon=int(sys.argv[1]))
